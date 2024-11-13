edf291024d74019a30875cfb81f649e18c3c5e8a
Aron <rabararon@gmail.com>
2024-11-13 10:10:55 +0100

    Initialize Project

---
+++ sj24-25-Scherzer-Rabar/src/main/java/spengergasse/at/sj2425scherzerrabar/Sj2425ScherzerRabarApplication.java
@@ -0,0 +1,13 @@
+package spengergasse.at.sj2425scherzerrabar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sj2425ScherzerRabarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sj2425ScherzerRabarApplication.class, args);
+    }
+
+}
