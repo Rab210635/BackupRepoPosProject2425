22f71b72017d894759e63a42cea1fb37cef6acd9
sch6 <116423267+sch6@users.noreply.github.com>
2024-12-07 19:12:12 +0100

    BookTypeConverter BookTypeConverter Tests

--- sj24-25-Scherzer-Rabar/src/main/java/spengergasse/at/sj2425scherzerrabar/domain/Copy.java
+++ sj24-25-Scherzer-Rabar/src/main/java/spengergasse/at/sj2425scherzerrabar/domain/Copy.java
@@ -3,6 +3,7 @@ package spengergasse.at.sj2425scherzerrabar.domain;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.domain.AbstractPersistable;
+import spengergasse.at.sj2425scherzerrabar.persistence.converter.BookTypeConverter;
 
 @Entity
 @Table(name = "copy")
@@ -13,6 +14,7 @@ public class Copy {
     @ManyToOne(fetch = FetchType.LAZY)
     private Publisher publisher;
 
+    @Column(columnDefinition = BookTypeConverter.COLUMN_DEFINITION)
     private BookType bookType;
     private Integer pageCount;
     @ManyToOne(fetch = FetchType.LAZY)
