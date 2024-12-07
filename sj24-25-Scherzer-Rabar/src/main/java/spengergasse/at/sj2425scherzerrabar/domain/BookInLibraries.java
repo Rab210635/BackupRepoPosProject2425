6d5ac8e2ab78d9ea8c7b3737b4492dde39787519
sch6 <116423267+sch6@users.noreply.github.com>
2024-12-07 23:31:41 +0100

    BookTypeConverter BookTypeConverter Tests Subscribing BookInLibraries Branch Branch Tests Library Library Tests Publisher Publisher Tests

--- sj24-25-Scherzer-Rabar/src/main/java/spengergasse/at/sj2425scherzerrabar/domain/BookInLibraries.java
+++ sj24-25-Scherzer-Rabar/src/main/java/spengergasse/at/sj2425scherzerrabar/domain/BookInLibraries.java
@@ -1,6 +1,13 @@
 package spengergasse.at.sj2425scherzerrabar.domain;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 @Embeddable
 public class BookInLibraries {
